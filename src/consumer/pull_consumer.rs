15fa5ef80a243918a06842263c9ee1be7bee6f88
rust <rust@t.t>
2024-10-22 15:39:07 +0800

    0.3.4

--- src/consumer/pull_consumer.rs
+++ src/consumer/pull_consumer.rs
@@ -333,7 +333,7 @@ impl MqConsumer {
                         tx.send(cmd).await.unwrap();
                     }
                 }
-                Self::sleep(10).await;
+                Self::sleep(499).await;
             }
         });
     }
